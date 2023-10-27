6436fd07c04565eb207d3820e46e50d7ea58bf18
Oluwatosin Faith Ogungbaye <hellotosinfaith@gmail.com>
2023-10-27 23:13:19 +0100

    upload

---
+++ src/DecryptionApplication.java
@@ -0,0 +1,32 @@
+/*
+
+Task: A plaintext was encrypted with a Caesar cipher with a shift of 7 (A maps to H). The resulting ciphertext is:
+Kvu'a qbknl h ivvr if paz jvcly. What was the original plaintext?
+
+Answer: DON'T JUDGE A BOOK BY IT'S COVER
+
+ */
+
+public class DecryptionApplication {
+    public static void main(String[] args) {
+
+
+    }
+
+   public String decrpytUserInput(String userInput){ // APPLE
+       StringBuilder stringBuilder = new StringBuilder();
+       String[] splitUserInput = userInput.split("");
+
+       for (int i = 0; i < userInput.length(); i++) {
+           for (int j = 0; j < storeInput[i].length(); j++) {
+//
+//           }       }
+//       return decrpytedUserInput;
+//    }
+//
+//}
+
+
+
+
+    }
